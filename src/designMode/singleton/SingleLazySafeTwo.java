9df1f3a8a91917ebc9e2bd89c7f1ef013817d59e
wee616 <1191064950@qq.com>
2018-07-09 22:57:03 +0800

    Update SingleLazySafeTwo.java

--- src/designMode/singleton/SingleLazySafeTwo.java
+++ src/designMode/singleton/SingleLazySafeTwo.java
@@ -9,13 +9,14 @@ public class SingleLazySafeTwo {
 	//构造函数定义为私有的，防止被调用
 	private SingleLazySafeTwo(){};
 	
-	private static SingleLazySafeTwo instance;
+	private static volatile SingleLazySafeTwo instance;
 	
 	//若instance为空，进行初始化，运行时初始化，代码块加synchronized线程安全，效率高
 	public static SingleLazySafeTwo getInstance(){
 		if(instance == null){
 			synchronized(SingleLazySafeTwo.class){
 				if(instance == null){
+					//此处由于new 不是原子性动作，因此instance需要用volatile进行修饰
 					instance = new SingleLazySafeTwo();
 				}
 			}
